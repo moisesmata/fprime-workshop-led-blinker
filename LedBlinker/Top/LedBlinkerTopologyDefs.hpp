b5e83a709dc3bdbdbd620d19f85b0df5a4d9bce9
Moises Mata <mm6155@columbia.edu>
2025-07-31 14:54:23 -0700

    Fix LedBlinkerTopologyDefs.hpp, for subtopology instance usage

--- LedBlinker/Top/LedBlinkerTopologyDefs.hpp
+++ LedBlinker/Top/LedBlinkerTopologyDefs.hpp
@@ -11,24 +11,12 @@
 #include "Svc/FramingProtocol/FprimeProtocol.hpp"
 #include "Svc/Health/Health.hpp"
 
-// Subtopology PingEnties Includes
+// Subtopology PingEntries Includes
 #include "EventLoggerTee/PingEntries.hpp"
 #include "TlmLoggerTee/PingEntries.hpp"
 
-// Definitions are placed within a namespace named after the deployment
-namespace LedBlinker {
-
-/**
- * \brief required type definition to carry state
- *
- * The topology autocoder requires an object that carries state with the name `LedBlinker::TopologyState`. Only the type
- * definition is required by the autocoder and the contents of this object are otherwise opaque to the autocoder. The contents are entirely up
- * to the definition of the project. Here, they are derived from command line inputs.
- */
-struct TopologyState {
-    const CHAR* hostname;
-    U16 port;
-};
+// Include autocoded FPP constants
+#include "LedBlinker/Top/FppConstantsAc.hpp"
 
 /**
  * \brief required ping constants
@@ -50,45 +38,38 @@ struct TopologyState {
  * ```
  */
 namespace PingEntries {
-namespace LedBlinker_blockDrv {
-enum { WARN = 3, FATAL = 5 };
-}
-namespace LedBlinker_tlmSend {
-enum { WARN = 3, FATAL = 5 };
-}
-namespace LedBlinker_cmdDisp {
-enum { WARN = 3, FATAL = 5 };
-}
-namespace LedBlinker_cmdSeq {
-enum { WARN = 3, FATAL = 5 };
-}
-namespace LedBlinker_eventLogger {
-enum { WARN = 3, FATAL = 5 };
-}
-namespace LedBlinker_fileDownlink {
-enum { WARN = 3, FATAL = 5 };
-}
-namespace LedBlinker_fileManager {
-enum { WARN = 3, FATAL = 5 };
-}
-namespace LedBlinker_fileUplink {
-enum { WARN = 3, FATAL = 5 };
-}
-namespace LedBlinker_prmDb {
-enum { WARN = 3, FATAL = 5 };
-}
-namespace LedBlinker_rateGroup1 {
-enum { WARN = 3, FATAL = 5 };
-}
-namespace LedBlinker_rateGroup2 {
-enum { WARN = 3, FATAL = 5 };
-}
-namespace LedBlinker_rateGroup3 {
-enum { WARN = 3, FATAL = 5 };
-}
+    namespace LedBlinker_blockDrv      {enum { WARN = 3, FATAL = 5 };}
+    namespace LedBlinker_tlmSend       {enum { WARN = 3, FATAL = 5 };}
+    namespace LedBlinker_cmdDisp       {enum { WARN = 3, FATAL = 5 };}
+    namespace LedBlinker_cmdSeq        {enum { WARN = 3, FATAL = 5 };}
+    namespace LedBlinker_eventLogger   {enum { WARN = 3, FATAL = 5 };}
+    namespace LedBlinker_fileDownlink  {enum { WARN = 3, FATAL = 5 };}
+    namespace LedBlinker_fileManager   {enum { WARN = 3, FATAL = 5 };}
+    namespace LedBlinker_fileUplink    {enum { WARN = 3, FATAL = 5 };}
+    namespace LedBlinker_prmDb         {enum { WARN = 3, FATAL = 5 };}
+    namespace LedBlinker_rateGroup1    {enum { WARN = 3, FATAL = 5 };}
+    namespace LedBlinker_rateGroup2    {enum { WARN = 3, FATAL = 5 };}
+    namespace LedBlinker_rateGroup3    {enum { WARN = 3, FATAL = 5 };}
 }  // namespace PingEntries
 
-namespace PingEntries = ::PingEntries;
+// Definitions are placed within a namespace named after the deployment
+namespace LedBlinker {
+
+    /**
+     * \brief required type definition to carry state
+     *
+     * The topology autocoder requires an object that carries state with the name `LedBlinker::TopologyState`. Only the type
+     * definition is required by the autocoder and the contents of this object are otherwise opaque to the autocoder. The
+     * contents are entirely up to the definition of the project. This deployment uses EventLoggerTee and TlmLoggerTee
+     * subtopologies for ComLogger functionality.
+     */
+    struct TopologyState {
+        const char* hostname;   //!< Hostname for TCP communication
+        U16 port;              //!< Port for TCP communication
+    };
+
+    namespace PingEntries = ::PingEntries;
 
 }  // namespace LedBlinker
+
 #endif
